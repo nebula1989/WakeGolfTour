ebf90e77cc968fc3ae9862976281750ec74d8a61
nebula1989 <benwalterscoding@gmail.com>
2022-09-09 13:15:50 -0700

    updated IP to static

--- wgt_site/settings/prod.py
+++ wgt_site/settings/prod.py
@@ -3,6 +3,6 @@ from .base import *
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = ['44.192.129.152']
+ALLOWED_HOSTS = ['54.90.219.106']
 
 STATIC_ROOT = "/home/ubuntu/WakeGolfTour/static/"
