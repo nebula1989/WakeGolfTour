455fd565fa0e676b5d13c10bd17ac7e3b90030c1
nebula1989 <benwalterscoding@gmail.com>
2022-10-20 10:30:18 -0700

    updated domain to allowed hosts

--- wgt_site/settings/prod.py
+++ wgt_site/settings/prod.py
@@ -3,6 +3,6 @@ from .base import *
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = ['54.90.219.106']
+ALLOWED_HOSTS = ['54.90.219.106', 'waketour.golf', 'www.waketour.golf']
 
 STATIC_ROOT = "/home/ubuntu/WakeGolfTour/static/"
